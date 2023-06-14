8c138a14491269fa243c15bb88cabe8de07ad839
Julián Rojas <juliandra140201@gmail.com>
2023-06-14 14:22:26 -0500

    Hola Mundo en React!

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+//Seleccionamos el DOM y lo guardamos en una constante
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>Hola Mundo en React!!!</h1>);
+
