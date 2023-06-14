6798e7da259b6e9e4d11a1752a96488d5af60f0f
Julián Rojas <juliandra140201@gmail.com>
2023-06-14 14:47:31 -0500

    Ejemplo de un componente en React

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 //Seleccionamos el DOM y lo guardamos en una constante
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hola Mundo en React!!!</h1>);
+//Crear componentes a partir de funciones
+function Greeting () {
+    return <div>
+        <h1>Esto es un componente</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+    </div>
+}
+root.render(<div>
+    <Greeting />
+</div>)
 
