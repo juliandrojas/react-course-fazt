a514352340bf5ff72f4e8df904a891e0f582c4d7
Julián Rojas <juliandra140201@gmail.com>
2023-06-17 16:56:10 -0500

    propTypes & defaultProps

--- src/index.js
+++ src/index.js
@@ -1,61 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 //Importamos Component.js
-//import { Component, UserCard } from './Component';
-//import Product, { Navbar } from './Product';
-//import Greeting from "./Greeting";
-import { UserCard } from "./Component";
+import { Button } from './Button';
 //Seleccionamos el DOM y lo guardamos en una constante
 const root = ReactDOM.createRoot(document.getElementById("root"));
-/*
-//Crear componentes a partir de funciones
-function Greeting() {
-  //Para interpretar código JS dentro de HTML se usan las llaves
-  const name = "fazt";
-  return <h1>{name}</h1>;
-}*/
-function IsMarried() {
-  const married = false;
-  /*if(married) {
-        return <h1>Estoy casado</h1>
-    } else {
-        return <h1>No estoy casado</h1>
-    }*/
-  //Condicional con operador ternario
-  return <h1>{married ? "Estoy casado 😀" : "No estoy casado 🤐"}</h1>;
-}
-function Objects() {
-  const user = {
-    firstName: "Ryan",
-    lastname: "Ray",
-  };
-  //Convertimos a String el objeto user usando JSON.stringify()
-  return (
-    <div>
-      {/* <h1>{JSON.stringify(user)}</h1> */}
-      <h1>{user.firstName}</h1>
-      <h3>{user.lastname}</h3>
-    </div>
-  );
-}
-function IsMarriedString() {
-  const married = true;
-  return (
-    //Para convertir de booleano a String, usamos el método toString
-    <div>
-      <h1>{married.toString()}</h1>
-    </div>
-  );
-}
-//Función adentro de una función
-function PrimeraFuncion() {
-  function sumar(x, y) {
-    return x + y;
-  }
-  return <h1>{sumar(10, 30)}</h1>;
-}
-//Función importada
-
 //Usamos Fragment para evitar los divs
 root.render(
   <>
@@ -68,7 +16,10 @@ root.render(
     <Greeting y = {30}/>
     <Greeting z = {true} />
     <Greeting a = {[1, 2, 3]} /> */}
-    <UserCard
+    <Button text = "Click me"/>
+    <Button text = "Pay"/>
+    <Button text =  "Save" name="Joe" />
+    {/* <UserCard
       name="Ryan Ray"
       amount={3000}
       married={true}
@@ -82,6 +33,6 @@ root.render(
       married={false}
       points={[100, 20]}
       address={{ street: "Avenue Some 123", city: "New York" }} 
-    />
+    /> */}
   </>
 );
