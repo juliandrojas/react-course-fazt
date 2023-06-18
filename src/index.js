4d0013f4d780ac13d409bb8ba85201d41a0826a9
Julián Rojas <juliandra140201@gmail.com>
2023-06-18 09:47:12 -0500

    Classic React (Components using classes)

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 //Importamos Component.js
 import { Button } from './Button';
 import { TaskCard } from './Task';
+import { Saludar } from './Saludar';
 //Seleccionamos el DOM y lo guardamos en una constante
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //Usamos Fragment para evitar los divs
 root.render(
   <>
    <TaskCard ready = {true} />
+   <Saludar />
   </>
 );
