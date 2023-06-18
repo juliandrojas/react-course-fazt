52ec556cbb41bd3b69ed9f1de13a1d10cab55102
Julián Rojas <juliandra140201@gmail.com>
2023-06-18 13:03:25 -0500

    Installation and use of React-icons library

--- src/Post.js
+++ src/Post.js
@@ -1,7 +1,8 @@
+import { VscGlobe } from 'react-icons/vsc';
 export const Post = () => {
     return <button onClick={() => {
         //Pedimos los datos a la API
-        fetch('https://jsonplaceholder.typicode.xyz/posts')
+        fetch('https://jsonplaceholder.typicode.com/posts')
             //Convertimos los datos a JSON
             .then(response => response.json())
             //Los mostramos por consola
@@ -9,6 +10,7 @@ export const Post = () => {
             //Si ocurre un error (la URL no existe)
             .catch(error => console.error(error))
     }}>
+        <VscGlobe />
         Traer datos
     </button>
 }
