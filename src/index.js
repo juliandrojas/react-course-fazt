4bb875e1b7159be00dea21531f0427783facd99e
Julián Rojas <juliandra140201@gmail.com>
2023-06-20 14:27:33 -0500

    How to use the UseEffect hook

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 //Importamos Post.js
 //import { Post } from './Post';
@@ -25,12 +25,24 @@ function Counter() {
 }
 function CounterInput() {
   const [ message, setMessage ] = useState('')
+  const [ counter, setCounter] = useState(0)
+  //Forma menos común de encontrar un useEffect y se ejecutará al cargar o actualizar el componente 
+  useEffect(() => {console.log("Cargando componente")})
+  //Indicamos con un arreglo vacío para que el useEffect se ejecute al principio del renderizado
+  useEffect(() => {console.log("Componente vacío")}, []);
+  //O indicamos con el counter (contador del useState) para que se ejecute el useEffect
+  useEffect(() => {
+    console.log("Renderizando componente");
+  }, [counter]);
   return(
     <div>
       <input onChange={e => setMessage(e.target.value)}/>
       <button onClick={() => {
         alert("El mensaje es: "+message)
         }}>Save</button>
+        <hr />
+        <h1>Counter: {counter}</h1>
+        <button onClick={() => setCounter(counter + 1)}>Incrementar</button>
     </div>
   )
 }
