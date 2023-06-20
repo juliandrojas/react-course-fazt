32e209836d74272ec8b434a2f7d9ec18555e31b0
Julián Rojas <juliandra140201@gmail.com>
2023-06-20 10:50:14 -0500

    Arrays in React

--- src/index.js
+++ src/index.js
@@ -4,9 +4,35 @@ import ReactDOM from "react-dom/client";
 import { Post } from './Post';
 //Seleccionamos el DOM y lo guardamos en una constante
 const root = ReactDOM.createRoot(document.getElementById("root"));
+//Arreglo de usuarios
+const users = [
+  {
+    id: 1, 
+    name: 'Ryan Ray',
+    image: 'https://robohash.org/user1'
+  },
+  {
+    id: 2, 
+    name: 'Joe',
+    image: 'https://robohash.org/user2'
+  },
+  {
+    id: 3, 
+    name: 'Marcos',
+    image: 'https://robohash.org/user3'
+  }
+]
+
 //Usamos Fragment para evitar los divs
 root.render(
   <>
-    <Post />
+    {users.map((user, i) => {
+      return (
+        <div key={i}>
+          <h1> {user.name} </h1>
+          <img src={user.image}/>
+        </div>
+      );
+    })}
   </>
 );
