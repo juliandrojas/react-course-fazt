7524acb631f18eccb53c216326eddb1aefc1d13e
Julián Rojas <juliandra140201@gmail.com>
2023-06-20 14:06:22 -0500

    example of useState Hook

--- src/index.js
+++ src/index.js
@@ -1,38 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 //Importamos Post.js
-import { Post } from './Post';
+//import { Post } from './Post';
 //Seleccionamos el DOM y lo guardamos en una constante
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//Arreglo de usuarios
-const users = [
-  {
-    id: 1, 
-    name: 'Ryan Ray',
-    image: 'https://robohash.org/user1'
-  },
-  {
-    id: 2, 
-    name: 'Joe',
-    image: 'https://robohash.org/user2'
-  },
-  {
-    id: 3, 
-    name: 'Marcos',
-    image: 'https://robohash.org/user3'
-  }
-]
-
+function Counter() {
+  //Ponemos dentro del useState el valor inicial del counter
+  const [counter, setCounter ] = useState(0)
+  return(
+    <div>
+      <h1>Counter: { counter }</h1>
+      <button onClick={() => {
+        //Establecemos un contador 
+        setCounter(counter + 1)
+      }}>Sumar</button>
+      <button onClick={() => {
+        setCounter(counter - 1)
+      }}>Restar</button>
+      <button onClick={() => {
+        setCounter(0)
+      }}>Reiniciar</button>
+    </div>
+  )
+}
+function CounterInput() {
+  const [ message, setMessage ] = useState('')
+  return(
+    <div>
+      <input onChange={e => setMessage(e.target.value)}/>
+      <button onClick={() => {
+        alert("El mensaje es: "+message)
+        }}>Save</button>
+    </div>
+  )
+}
 //Usamos Fragment para evitar los divs
 root.render(
   <>
-    {users.map((user, i) => {
-      return (
-        <div key={i}>
-          <h1> {user.name} </h1>
-          <img src={user.image}/>
-        </div>
-      );
-    })}
+    <CounterInput />
   </>
 );
